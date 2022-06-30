93f2864436fff782d61dc05f37f2cbd957432aa4
Phu <ptranny@gmail.com>
2022-06-30 17:44:46 +1200

    Add styles and skeleton HTML for components

--- client/components/Classes.jsx
+++ client/components/Classes.jsx
@@ -1,5 +1,31 @@
 import React from 'react'
 
 export default function Classes() {
-  return <p>Classes</p>
+  return (
+    <div className="classes">
+      <table className="classes-table">
+        <thead>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th></th>
+        </thead>
+        <tr>
+          <td>11/02/2022</td>
+          <td>6:30pm</td>
+          <td>Sleepy As</td>
+          <td>Relaxing yin yoga</td>
+          <td>Book</td>
+        </tr>
+        <tr>
+          <td>11/02/2022</td>
+          <td>6:30am</td>
+          <td>Sweaty As</td>
+          <td>Work up a sweat</td>
+          <td>Book</td>
+        </tr>
+      </table>
+    </div>
+  )
 }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <>
-      <h1>Sweet As Yoga</h1>
-      <img className="landing-image" src=""></img>
-      <div>Description</div>
-    </>
+    <div className="home">
+      <img
+        className="landing-image"
+        src="/images/landing.jpg"
+        alt="Yogi on cliff"
+      ></img>
+      <div className="home-description">
+        This little oasis tucked in amongst the hustle and bustle of Newmarket
+        is a place to relax, find your inner peace, get energised and feel
+        balanced. Sweet As Yoga is a welcoming studio for people at all stages
+        of their yoga journey, whether it is your first class or you have been
+        practising for years. They also have regular workshops and special
+        events on the calendar where you can get a more in-depth practice, or
+        learn new skills from the best of the best.
+      </div>
+    </div>
   )
 }
