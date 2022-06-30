c8c9c3a238c56ae04f2773f8f36d3cfb0d339e6c
yiyuchen-nz <yiyuchen9@gmail.com>
2022-06-30 17:21:21 +1200

    setup database and seed

--- server/db/seeds/classes.js
+++ server/db/seeds/classes.js
@@ -1,12 +1,54 @@
-// exports.seed = function (knex) {
-//   // Deletes ALL existing entries
-//   return knex('fruit').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('fruit').insert([
-//         { id: 1, name: 'banana' },
-//         { id: 2, name: 'apple' },
-//         { id: 3, name: 'feijoa' }
-//       ])
-//     })
-// }
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('classes').del()
+  await knex('classes').insert([
+    {
+      id: 1,
+      name: 'Sweaty As',
+      classDescription: 'A very sweaty yoga experience!',
+      dateTime: 'June 30th 2022, 4:30:00 pm',
+    },
+    {
+      id: 2,
+      name: 'Sleepy As',
+      classDescription: 'A very sleepy yoga experience!',
+      dateTime: 'July 1st 2022, 10:00:00 am',
+    },
+    {
+      id: 3,
+      name: 'Bendy As',
+      classDescription: 'A very bendy yoga experience!',
+      dateTime: 'July 1st 2022, 3:00:00 pm',
+    },
+    {
+      id: 4,
+      name: 'Inverted As',
+      classDescription: 'A very inverted yoga experience!',
+      dateTime: 'July 2nd 2022, 4:00:00 pm',
+    },
+    {
+      id: 5,
+      name: 'Sweaty As',
+      classDescription: 'A very sweaty yoga experience!',
+      dateTime: 'July 3rd 2022, 4:00:00 pm',
+    },
+    {
+      id: 6,
+      name: 'Sleepy As',
+      classDescription: 'A very sleepy yoga experience!',
+      dateTime: 'July 4th 2022, 4:00:00 pm',
+    },
+    {
+      id: 7,
+      name: 'Bendy As',
+      classDescription: 'A very bendy yoga experience!',
+      dateTime: 'July 5th 2022, 4:00:00 pm',
+    },
+    {
+      id: 8,
+      name: 'Inverted As',
+      classDescription: 'A very inverted yoga experience!',
+      dateTime: 'July 6th 2022, 4:00:00pm',
+    },
+  ])
+}
