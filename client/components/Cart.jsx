b1f8b1e11c338a10a7ba3816e70597432bc8cd03
mano-pihema <mano-pihema-laptop@LAPTOP-MNKDSN6R.localdomain>
2022-07-01 12:27:52 +1200

    i am not a bug

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+//import { Link } from 'react-router-dom'
+
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function Cart() {
+  //const [order,setOrder] = useState([])
+  const dispatch = useDispatch()
+  const order = useSelector((state) => state.cart)
+  console.log('order', order)
+
+  useEffect(async () => {}, [])
+
   return (
     <div className="cart">
       <table className="cart-table">
@@ -11,18 +20,16 @@ export default function Cart() {
           <th>Name</th>
           <th></th>
         </thead>
-        <tr>
-          <td>11/02/2022</td>
-          <td>6:30pm</td>
-          <td>Sleepy As</td>
-          <td>Remove</td>
-        </tr>
-        <tr>
-          <td>11/02/2022</td>
-          <td>6:30am</td>
-          <td>Sweaty As</td>
-          <td>Remove</td>
-        </tr>
+        {order.map((item) => {
+          return (
+            <tr key={item.id}>
+              <td>{item.dateTime}</td>
+              <td>{item.dateTime}</td>
+              <td>{item.name}</td>
+              <td>Remove</td>
+            </tr>
+          )
+        })}
       </table>
 
       <button>Submit</button>
